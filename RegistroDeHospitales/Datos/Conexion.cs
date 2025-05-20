51985d453a4fb7bd7eb1bc61eede8d6e7f54ddb1
Juan Pablo Saldarriaga González <juan.saldarriaga237@pascualbravo.edu.co>
2025-05-20 17:18:35 -0500

    Agregar a la carpeta datos, la clase "Conexion.cs"

---
+++ RegistroDeHospitales/Datos/Conexion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace RegistroDeHospitales.Datos
+{
+    public class Conexion
+    {
+        private static SqlConnection conexion;
+        private static readonly string cadena = "Server=localhost\\SQLEXPRESS;Database=RegistroDeHospitales;Trusted_Connection=True;";
+
+        public static SqlConnection ObtenerConexion()
+        {
+            if (conexion == null)
+                conexion = new SqlConnection(cadena);
+
+            return conexion;
+        }
+    }
+}
