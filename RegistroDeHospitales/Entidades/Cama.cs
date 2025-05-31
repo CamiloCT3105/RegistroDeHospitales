f5182ac183cb01e80187c6a404d6519c31dd950e
ACardonaK <kassandra.ariza420@pascualbravo.edu.co>
2025-05-30 20:48:49 -0500

    CRUD Salas y camas

---
+++ RegistroDeHospitales/Entidades/Cama.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+    namespace RegistroDeHospitales.Modelos
+{
+    public class Cama
+    {
+        public int CamaID { get; set; }
+        public string Numero { get; set; }
+        public string Especializacion { get; set; } // General, UCI, etc.
+        public string Estado { get; set; } // Disponible / Ocupada
+        public int SalaID { get; set; } // Relación con sala
+    }
+}
+}
